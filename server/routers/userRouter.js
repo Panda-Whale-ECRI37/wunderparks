b01375459e38af036ced408190fa3acd91636b58
Alan Perng <aperng31@gmail.com>
2022-12-14 14:17:51 -0800

    Change schema, update get trip, add trip, add parks controllers

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,16 +3,16 @@ const userController = require('../controllers/UserController');
 
 const userRouter = express.Router();
 
-userRouter.get(
+userRouter.get( //body: {username}
   '/:parkCode',
   userController.getUser,
   userController.getParkInfo,
   (_req, res) => {
-    return res.status(200).json(res.locals.parkInfo);
+    return res.status(200).send(res.locals.tripsInfo); //res.locals.parkInfo
   }
 );
 
-userRouter.post(
+userRouter.post( //body: {username, password}
   '/login',
   userController.verifyUser,
   userController.getParks,
@@ -21,11 +21,13 @@ userRouter.post(
   }
 );
 
-userRouter.post('/:parkCode', userController.addPark, (_req, res) => {
-  return res.status(200).json(res.locals.parks);
+userRouter.post( //body: {username, date, notes, activitiesDone}
+  '/:parkCode',
+  userController.addPark, userController.addTrip, (_req, res) => {
+  return res.status(200).json({ parks: res.locals.parks, trips: res.locals.trips });
 });
 
-userRouter.post(
+userRouter.post( //body: {name, username, password}
   '/',
   userController.getUser,
   userController.createUser,
