06acbd947c0380c8eed3bd2291114bf12f24912f
kimballcaroline <kimballcaroline@gmail.com>
2022-12-14 12:42:30 -0500

    mapped out filter structure

--- client/components/IconMaker.jsx
+++ client/components/IconMaker.jsx
@@ -1,36 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import Icon from './Icon.jsx';
+import React, { useState, useEffect } from "react";
+import Icon from "./Icon.jsx";
 
 // import in the images.js file
-import images from '../public/images.js';
-import parkCodes from '../public/parkcodes.js';
+import images from "../public/images.js";
+import parkCodes from "../public/parkcodes.js";
 
 function IconMaker(props) {
   const { codes } = props;
   // console.log('props in iconmaker: ', props);
+
+  // const [amenities, setAmenities] = useState(
+  //   {
+  //     toilets: false,
+  //     maps: false,
+  //     picnicTables: false
+  // });
+
   const parksArr = [];
   for (let park in images) {
     let parkCode;
-
+    //if(park matches filters...then do all of this stuff)
     Object.keys(parkCodes).forEach((element) => {
       if (element.toLowerCase().includes(park.toLowerCase())) {
         parkCode = parkCodes[element];
         // console.log(codes.includes(parkCode));
       }
     });
+
     parksArr.push(
       <Icon
         key={park}
         park={park}
         imgLink={images[park]}
         parkCode={parkCode}
-        className={codes.includes(parkCode) ? 'color' : undefined}
+        className={codes.includes(parkCode) ? "color" : undefined}
       />
     );
     // console.log('link :', imgLink/)
   }
 
-  return <div className="container">{parksArr}</div>;
+  return <div className='container'>{parksArr}</div>;
 }
 
 export default IconMaker;
