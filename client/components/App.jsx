df675506eeda0cdcb3f3cff817fd711b735e46d8
kimballcaroline <kimballcaroline@gmail.com>
2022-12-14 12:42:16 -0500

    prettier made everything double quotes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
-import SidebarContainer from '../containers/SidebarContainer.jsx';
-import MainContainer from '../containers/MainContainer.jsx';
-import CreateUser from './modal/CreateUser.jsx';
-import LogIn from './modal/LogIn.jsx';
-import Welcome from './modal/Welcome.jsx';
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import SidebarContainer from "../containers/SidebarContainer.jsx";
+import MainContainer from "../containers/MainContainer.jsx";
+import CreateUser from "./modal/CreateUser.jsx";
+import LogIn from "./modal/LogIn.jsx";
+import Welcome from "./modal/Welcome.jsx";
 
 const App = () => {
   // let codes = [];
@@ -14,15 +14,15 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetch('http://localhost:3000/user/', {
-      method: 'GET',
-      headers: { 'Content-Type': 'Application/JSON' },
+    fetch("http://localhost:3000/user/", {
+      method: "GET",
+      headers: { "Content-Type": "Application/JSON" },
     })
       .then((res) => res.json())
       .then((data) => {
         setCodes(data);
       })
-      .catch((err) => console.log('AddPark fetch POST to api: ERROR: ', err));
+      .catch((err) => console.log("AddPark fetch POST to api: ERROR: ", err));
   }, []);
 
   // States for rendering the different compontents
@@ -30,6 +30,7 @@ const App = () => {
   const [showCreateAccount, setCreateAccount] = useState(false);
   const [showLogIn, setLogIn] = useState(false);
   const [showWebsite, setShowWebsite] = useState(false);
+  const [planOptions, setPlanOptions] = useState();
 
   const renderToPage = () => {
     if (welcome === true) {
@@ -46,8 +47,8 @@ const App = () => {
       return <LogIn />;
     } else if (showWebsite === true) {
       return (
-        <div className="right">
-          <div className="float">
+        <div className='right'>
+          <div className='float'>
             <h1> WÜNDER PARKS</h1>
           </div>
           <MainContainer codes={codes} />
@@ -60,25 +61,25 @@ const App = () => {
 
   function showCorrectModal(component) {
     setWelcome(false);
-    if (component === 'showCreateAccount') {
+    if (component === "showCreateAccount") {
       setCreateAccount(true);
-    } else if (component === 'showLogIn') {
+    } else if (component === "showLogIn") {
       setLogIn(true);
     }
   }
 
   return (
-    <div className="app">
-      {renderToPage()}
+    <div className='app'>
+      {/* {renderToPage()} */}
       {/* <LogIn /> */}
       {/* <CreateUser /> */}
-      {/* <SidebarContainer codes={codes} />
-      <div className="right">
-        <div className="float">
+      <SidebarContainer codes={codes} />
+      <div className='right'>
+        <div className='float'>
           <h1> WÜNDER PARKS</h1>
         </div>
         <MainContainer codes={codes} />
-      </div> */}
+      </div>
     </div>
   );
 };
